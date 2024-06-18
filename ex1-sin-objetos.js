3201456f69d3bc5b4fe32f0b60ec90795e9e0c4f
SharmarAndrey <sharnarandrey@gmail.com>
2024-06-18 22:04:34 +0200

    echo sin ultimos algoritmos

--- ex1-sin-objetos.js
+++ ex1-sin-objetos.js
@@ -9,24 +9,38 @@ const numbers = [-5, -3, -1, 1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
 /**
  * Utiliza la método forEach para mostrar cada número por consola
  */
-
+numbers.forEach((number) => console.log("Ejercicio 1:", number));
 /**
  * Utiliza el método find para encontrar el primer número mayor de 10
  */
-
+const mayor10 = numbers.find((n) => n > 10);
+console.log("Ejercicio 2:", mayor10);
 /**
  * Utliza el método some para saber si existe un número mayor de 20
  */
-
+const siExiste = numbers.some((n) => n > 20);
+console.log("Ejercicio 3:", siExiste);
 /**
  * Utiliza el método every para comprobar si todos los números de este array son impares
  */
-
+const elNumeroPar = numbers.every((n) => n % 2 !== 0);
+console.log("Ejercicio 4:", elNumeroPar);
 /**
  * Utiliza el método filter para obtener un nuevo array con todos los números que son mayores de 3 pero menores de 9
  */
-
+const newNumbers = numbers.filter((n) => 3 < n & n < 9);
+console.log("Ejercicio 5:", newNumbers)
 /**
  * Utiliza el método map para obtener otro array donde, para cada posición, ontengamos un -1 si el número que ocupaba aquella posición es negativo, y un +1 si el nuemero es positivo
  * [-1, -1, -1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
  */
+const otroArray = numbers.map(n => {
+	if (n > 0) {
+		return 1;
+	}
+	return -1;
+
+
+});
+console.log("Ejercicio 6:", otroArray);
+
--- ex2-sin-objetos.js
+++ ex2-sin-objetos.js
@@ -1,30 +1,43 @@
 const students = [
-  "Albertina",
-  "Katerina",
-  "Ester",
-  "Galis",
-  "Oriol",
-  "Susana",
-  "McCarmen",
-  "Gerard",
-  "Dayane",
-  "Susana",
-  "Diego",
-  "Maria",
+	"Albertina",
+	"Katerina",
+	"Ester",
+	"Galis",
+	"Oriol",
+	"Susana",
+	"McCarmen",
+	"Gerard",
+	"Dayane",
+	"Susana",
+	"Diego",
+	"Maria",
 ];
 
 // Resultado esperado: https://oscarm.tinytake.com/msc/ODg4Njg3NV8yMjM0MDcwNg
 
 // Utiliza el método forEach para mostrar cada elemento del array por la consola
-
+students.forEach(s => console.log(s)
+);
 // Utiliza el método findIndex para encontrar que índica ocupa el string "Maria"
+const indexOfStudent = students.findIndex(s => s == "Maria"); console.log('indica que ocupa el sting "MAria" es :', indexOfStudent);
 
 // Utiliza el método some para saber si en este array existe la alumna "Katerina"
+const nameExist = students.some(n => n == "Katerina");
+console.log('El nombre Katerina existe en el array ? ', nameExist);
 
 // Utiliza el método find para saber el primer nombre que tiene 5 o menos carácteres
+const nameWIthFiveLetters = students.find(s => s.length <= 5);
+console.log('el primer nombre que tiene 5 o menos carácteres :', nameWIthFiveLetters);
 
 // Utiliza el método every para comprobar si todos los strings de este array tienen 4 o más carácteres
+const stringsLength = students.every(s => s.length >= 4);
+console.log('todos los strings de este array tienen 4 o más carácteres ?', stringsLength);
 
 // Utiliza el método filter para obtener un nuevo array que solo incluya aquellos nombres que contengan la letra i
 
+const names_l = students.filter(s => s.includes("i"));
+console.log(names_l);
 // Utiliza el método map para obtener un nuevo array donde aparezca solamente la primera inicial de cada nombre
+
+const letrasIniciales = students.map(s => s.charAt(0));
+console.log(letrasIniciales);
